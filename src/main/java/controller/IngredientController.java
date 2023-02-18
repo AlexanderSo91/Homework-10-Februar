9a4cf13b7cb7e379d404dcbd76ef8a5ae2ab0107
zotin <zotinalexander@gmail.com>
2023-02-18 20:32:31 +0100

    commit done

--- src/main/java/controller/IngredientController.java
+++ src/main/java/controller/IngredientController.java
@@ -1,6 +1,8 @@
 package controller;
 
 
+import annotation.Operation;
+import annotation.Tag;
 import model.Ingredient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,34 +13,49 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/ingredient")
-
+@Tag(name = "Api ингредиенты", description = "CRUD операции")
 public class IngredientController {
     private final IngredientService ingredientService;
 
 
     public IngredientController(IngredientService ingredientService) {
         this.ingredientService = ingredientService;
-
     }
+    @Operation(
+            summary = "Сохранение ингредиента"
+    )
+
     @PostMapping
     public ResponseEntity<Ingredient> save(@RequestBody Ingredient ingredient) {
         return ResponseEntity.ok(ingredientService.save(ingredient));
     }
+    @Operation(
+            summary = "Получение ингредиента по id"
+    )
 
     @GetMapping("/{id}")
     public ResponseEntity<Ingredient> getById(@PathVariable Long id) {
         return ResponseEntity.of(ingredientService.getById(id));
     }
+    @Operation(
+            summary = "Обновление ингредиента"
+    )
 
     @PutMapping("/{id}")
     public ResponseEntity<Ingredient> update(@PathVariable Long id, @RequestBody Ingredient ingredient) {
         return ResponseEntity.ok(ingredientService.update(id, ingredient));
     }
+    @Operation(
+            summary = "Удаление ингредиента"
+    )
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Ingredient> delete(@PathVariable Long id) {
         return ResponseEntity.ok(ingredientService.delete(id));
     }
+    @Operation(
+            summary = "Получение ингридиентов"
+    )
 
     @GetMapping("/{id}")
     public ResponseEntity<Map<Long, Ingredient>> getAll() {
--- src/main/java/controller/RecipeController.java
+++ src/main/java/controller/RecipeController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import annotation.Operation;
+import annotation.Tag;
 import model.Recipe;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/recipe")
-
+@Tag(name = "Api рецепты", description = "CRUD рецепты")
 public class RecipeController {
 
     private final RecipeService recipeService;
@@ -20,26 +22,42 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @Operation(
+            summary = "Сохранение рецепта"
+    )
     @PostMapping
     public ResponseEntity<Recipe> save(@RequestBody Recipe recipe) {
-        Recipe recipeRs = recipeService.save(recipe);
-        return ResponseEntity.ok(recipeRs);
+        return ResponseEntity.ok(recipeService.save(recipe));
     }
+    @Operation(
+            summary = "Получение рецепта по id"
+    )
 
     @GetMapping("/{id}")
     public ResponseEntity<Recipe> getById(@PathVariable Long id) {
         return ResponseEntity.of(recipeService.getById(id));
     }
 
+    @Operation(
+            summary = "Обновление рецепта"
+    )
+
     @PutMapping("/{id}")
     public ResponseEntity<Recipe> update(@PathVariable Long id, @RequestBody Recipe ingredient) {
         return ResponseEntity.ok(recipeService.update(id,recipe));
     }
 
+    @Operation(
+            summary = "Удаление рецепта"
+    )
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Recipe> delete(@PathVariable Long id) {
         return ResponseEntity.ok(recipeService.delete(id));
     }
+    @Operation(
+            summary = "Получение рецепта"
+    )
 
     @GetMapping("/{id}")
     public ResponseEntity<Map<Long, Recipe>> getAll() {
